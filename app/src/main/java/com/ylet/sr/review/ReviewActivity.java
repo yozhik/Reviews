8ca20eb4d9d8e03cef0410d4f91986b1eff2478b
serhiiradkivskyi <sr@letyshops.ru>
2018-03-22 19:10:57 +0200

    First working version.

--- app/src/main/java/com/ylet/sr/review/ReviewActivity.java
+++ app/src/main/java/com/ylet/sr/review/ReviewActivity.java
@@ -1,7 +1,9 @@
 package com.ylet.sr.review;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ReviewActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_layout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+//        final ActionBar ab = getSupportActionBar();
+//        ab.setDisplayHomeAsUpEnabled(true);
+
     }
 }
