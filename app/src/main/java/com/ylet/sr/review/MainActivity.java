368983f1501d824a8967a92c491a6fea19103fa7
serhiiradkivskyi <sr@letyshops.ru>
2018-04-11 13:06:49 +0300

    Expanding layout. done.

--- app/src/main/java/com/ylet/sr/review/MainActivity.java
+++ app/src/main/java/com/ylet/sr/review/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ylet.sr.review;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,8 +23,20 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btn2)
     Button btn2;
 
-    @BindView(R.id.send_review_main_button)
-    CardView sendButton;
+    @BindView(R.id.expand_review_layout_btn)
+    CardView expandReviewLayoutBtn;
+
+    @BindView(R.id.expandable_layout)
+    LinearLayout expandableLayout;
+
+    @BindView(R.id.close_btn)
+    ImageView closeBtn;
+
+    @BindView(R.id.send_review_button)
+    CardView sendReviewBtn;
+
+    private int expandReviewLayoutBtnHeight = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +58,127 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        expandableLayout.setVisibility(View.GONE);
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        expandReviewLayoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int k = 0;
+                if (expandableLayout.getVisibility() == View.GONE) {
+                    expand();
+                }
+            }
+        });
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableLayout.getVisibility() == View.VISIBLE) {
+                    collapse();
+                }
+            }
+        });
+
+        sendReviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableLayout.getVisibility() == View.VISIBLE) {
+                    collapse();
+                }
+            }
+        });
+
+    }
+
+    private void collapse() {
+        int finalHeight = expandableLayout.getHeight();
+
+        ValueAnimator animator = slideAnimator(finalHeight, 0, expandableLayout);
+        ValueAnimator expandSendBtnAnimator = slideAnimator(0, expandReviewLayoutBtnHeight, expandReviewLayoutBtn);
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                expandReviewLayoutBtn.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //Height=0, but it set visibility to GONE
+                //expandReviewLayoutBtn.setVisibility(View.VISIBLE);
+                //expandReviewLayoutBtn.setVisibility(View.VISIBLE);
+                expandableLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
 
             }
         });
+
+        animator.start();
+        expandSendBtnAnimator.start();
+    }
+
+    private void expand() {
+        //set Visible
+        expandableLayout.setVisibility(View.VISIBLE);
+
+        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        expandableLayout.measure(widthSpec, heightSpec);
+
+        expandReviewLayoutBtnHeight = expandReviewLayoutBtn.getMeasuredHeight();
+
+        ValueAnimator animator = slideAnimator(0, expandableLayout.getMeasuredHeight(), expandableLayout);
+        ValueAnimator collapseSendBtnAnimator = slideAnimator(expandReviewLayoutBtnHeight, 0, expandReviewLayoutBtn);
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //Height=0, but it set visibility to GONE
+                expandReviewLayoutBtn.setVisibility(View.GONE);
+                expandableLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        animator.start();
+        collapseSendBtnAnimator.start();
+    }
+
+    private ValueAnimator slideAnimator(int start, int end, final View animationView) {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                //Update Height
+                int value = (Integer) valueAnimator.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = animationView.getLayoutParams();
+                layoutParams.height = value;
+                animationView.setLayoutParams(layoutParams);
+            }
+        });
+        return animator;
     }
 
     private void startActivity(boolean showOneView) {
