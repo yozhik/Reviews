12efd75002a128be27dbc597a07ec72b11d8164a
serhiiradkivskyi <sr@letyshops.ru>
2018-04-11 11:06:28 +0300

    Expanding button.

--- app/src/main/java/com/ylet/sr/review/MainActivity.java
+++ app/src/main/java/com/ylet/sr/review/MainActivity.java
@@ -3,6 +3,7 @@ package com.ylet.sr.review;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.btn2)
     Button btn2;
+
+    @BindView(R.id.send_review_main_button)
+    CardView sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(false);
+            }
+        });
+
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int k = 0;
+
             }
         });
     }
