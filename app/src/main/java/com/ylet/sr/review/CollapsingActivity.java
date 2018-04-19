b33400a590f197fdb78eed140b8cc012eff9fb0c
serhiiradkivskyi <sr@letyshops.ru>
2018-04-19 15:47:21 +0300

    Loading data.

--- app/src/main/java/com/ylet/sr/review/CollapsingActivity.java
+++ app/src/main/java/com/ylet/sr/review/CollapsingActivity.java
@@ -5,16 +5,20 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CollapsingActivity extends AppCompatActivity implements ChangeNetworkNotification {
-
 
     @BindView(R.id.shop_final_root_layout)
     CoordinatorLayout shop_final_root_layout;
@@ -34,18 +38,33 @@ public class CollapsingActivity extends AppCompatActivity implements ChangeNetwo
     private NetworkStateReceiver networkStateReceiver;
     private boolean isConnected;
 
+    protected Fragment currentFragment;
+    protected Fragment previousFragment;
+    protected FragmentManager fragmentManager;
+    private boolean dataLoading = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapsing);
         ButterKnife.bind(this);
+
+        fragmentManager = getSupportFragmentManager();
+
         networkStateReceiver = new NetworkStateReceiver(this);
 
         IntentFilter intentFilterForNetwork = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
         registerReceiver(networkStateReceiver, intentFilterForNetwork);
 
         initToolbar();
+        loadData();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilterForNetwork = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(networkStateReceiver, intentFilterForNetwork);
     }
 
     private void initToolbar() {
@@ -53,6 +72,46 @@ public class CollapsingActivity extends AppCompatActivity implements ChangeNetwo
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadData() {
+        dataLoading = true;
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(CollapsingActivity.this, "Data loaded.", Toast.LENGTH_SHORT).show();
+
+                        boolean showOneView = true;
+                        if (showOneView) {
+                            initSingleView();
+                        } else {
+                            initTabView();
+                        }
+                        dataLoading = false;
+                    }
+                });
+            }
+        });
+        t.start();
+    }
+
+    private void initSingleView() {
+        tabLayout.setVisibility(View.GONE);
+        viewPager.setVisibility(View.GONE);
+        showFragmentWithoutBackStack(R.id.shop_final_content_holder, new MenuFragment1());
+    }
+
+    private void initTabView() {
+
     }
 
     @Override
@@ -64,5 +123,26 @@ public class CollapsingActivity extends AppCompatActivity implements ChangeNetwo
     @Override
     public void networkStateIsChanged(boolean isConnected) {
         this.isConnected = isConnected;
+        if (isConnected) {
+            Toast.makeText(CollapsingActivity.this, "Connection received.", Toast.LENGTH_SHORT).show();
+            if (!dataLoading) {
+                loadData();
+            }
+        } else {
+            Toast.makeText(CollapsingActivity.this, "Connection lost.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void showFragmentWithoutBackStack(int containerViewId, Fragment fragment) {
+        previousFragment = currentFragment;
+        currentFragment = fragment;
+        String fragmentTag = fragment.getClass().getSimpleName();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        if (previousFragment != null) {
+            fragmentTransaction.hide(previousFragment);
+        }
+        fragmentTransaction.add(containerViewId, fragment, fragmentTag)
+                .commitAllowingStateLoss();
     }
 }
